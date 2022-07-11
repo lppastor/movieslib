e2e9a73c68c92f3adff6d24c3e6b6ae0a454791f
Luis Paulo Pastor <luispaulo_pastor@hotmail.com>
2022-07-11 14:57:24 -0300

    Finalização do Projeto

--- movieslib/src/App.jsx
+++ movieslib/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <h2>Movies Lib</h2>
       <Outlet/>
     </div>
   )
--- movieslib/src/components/NavBar.jsx
+++ movieslib/src/components/NavBar.jsx
@@ -1,16 +1,29 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+import './NavBar.css'
+import { useState } from 'react'
  
 function NavBar() {
+    const [search, setSearch] = useState("")
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(search);
+
+        if(!search) return
+
+        navigate(`/search?q=${search}`);
+        setSearch("");
+    }
+
     return (
         <nav id="navbar">
             <h2>
-            <Link to="/"><BiCameraMovie/>MoviesLib</Link>
+                <Link to="/"><BiCameraMovie/>MoviesLib</Link>
             </h2>
-            <Link to="/movie/1">Movie</Link>
-            <Link to="/search">Search</Link>
-            <form>
-                <input type="text" placeholder='Busque um filme'/>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder='Busque um filme' onChange={(e) => setSearch(e.target.value)} value={search}/>
                 <button type='submit'><BiSearchAlt2/></button>
             </form>
         </nav>
